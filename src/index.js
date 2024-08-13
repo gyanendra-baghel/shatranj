f857620f8929819b5bf572881d7cbfa64a1abcc3
Gyanendra Baghel <gyanendrabaghel633@gmail.com>
2024-08-13 15:07:02 +0530

    FEAT:Add destroy feature

--- src/index.js
+++ src/index.js
@@ -7,12 +7,12 @@ class Shataranj {
     if (!(elem instanceof HTMLElement)) {
       throw new Error("Invalid element Id: " + elemId);
     }
-    const canvas = document.createElement("canvas");
-    const timer1 = document.createElement("p");
-    const timer2 = document.createElement("p");
+    this.canvas = document.createElement("canvas");
+    this.timer1 = document.createElement("p");
+    this.timer2 = document.createElement("p");
 
-    this.timer = new Timer(timer1, timer2);
-    this.chess = new CanvasBoard(canvas, size);
+    this.timer = new Timer(this.timer1, this.timer2);
+    this.chess = new CanvasBoard(this.canvas, size);
 
     this.chess.on("move", () => {
       this.timer.switchPlayer();
@@ -26,16 +26,21 @@ class Shataranj {
       this.chess.announceWinner(team);
     });
 
-    timer1.className = "timer1";
-    timer2.className = "timer2";
+    this.timer1.className = "timer1";
+    this.timer2.className = "timer2";
 
-    elem.appendChild(timer1);
-    elem.appendChild(canvas);
-    elem.appendChild(timer2);
+    elem.appendChild(this.timer1);
+    elem.appendChild(this.canvas);
+    elem.appendChild(this.timer2);
   }
   init(time = 600) {
     this.timer.start(time);
     this.chess.init();
+  }
+  destroy() {
+    this.canvas.remove();
+    this.timer1.remove();
+    this.timer2.remove();
   }
 }
 
